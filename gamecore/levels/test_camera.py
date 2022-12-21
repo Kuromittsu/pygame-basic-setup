642db03acdebeb71cf5c8b43a3cd942bd51f53de
alfiras <alfibagus130@gmail.com>
2022-12-22 00:17:55 +0700

    set basic level

--- gamecore/levels/test_camera.py
+++ gamecore/levels/test_camera.py
@@ -1,4 +1,5 @@
 from gamecore.player import Player
+from gamecore.objects.box import Box
 
 
 class Test_camera():
@@ -11,8 +12,20 @@ class Test_camera():
     def setup(self):
         self.player = Player(self.core, (100, 100), (60, 80))
 
+        self.boxs = [
+            Box(self.core.surfaces.game, (300, 200), '#1e3799', (50, 50)),
+            Box(self.core.surfaces.game, (200, 200), '#1e3799', (50, 50)),
+            Box(self.core.surfaces.game, (400, 300), '#1e3799', (50, 50)),
+        ]
+
     def update(self):
         self.player.update()
 
+        for box in self.boxs:
+            box.update()
+
     def draw(self):
         self.player.draw()
+        
+        for box in self.boxs:
+            box.draw()
--- gamecore/player.py
+++ gamecore/player.py
@@ -10,26 +10,53 @@ class Player():
         self.rect = self.image.get_rect(center=position)
         self.position = Vector2(self.rect.center)
         self.move = Vector2(0, 0)
-        self.speed = 200
+        self.default_speed = {
+            'walk': 200,
+            'run': 400,
+        }
+        self.speed = self.default_speed['walk']
+        self.facing = Vector2()
 
         self.setup()
+    
+    def _event(self):
+        for evt in self.core.event:
+
+            if evt.type == pg.KEYDOWN: 
+                if evt.key == pg.K_LSHIFT:
+                    self._run()
+
+            if evt.type == pg.KEYUP:
+                if evt.key == pg.K_LSHIFT:
+                    self._walk()
+
+    def _walk(self):
+        self.speed = self.default_speed['walk']
+
+    def _run(self):
+        self.speed = self.default_speed['run']
+    
+    def _facing(self):
+        self.facing = self.move
 
     def _input(self):
         keys = pg.key.get_pressed()
         self.move.x = int(keys[pg.K_d]) - int(keys[pg.K_a])
         self.move.y = int(keys[pg.K_s]) - int(keys[pg.K_w])
+        self._facing()
         if self.move.magnitude() > 0:
             self.move = self.move.normalize()
 
     def _move(self):
         self.position += self.move * self.speed * self.core.delta_time
-        self.rect.center = round(self.position.x), round(self.position.y)
+        self.rect.center = [round(self.position.x), round(self.position.y)]
 
     def setup(self):
         self.image.fill('#474787')
 
     def update(self):
         self._input()
+        self._event()
         self._move()
 
     def draw(self):
